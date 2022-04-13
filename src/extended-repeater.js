ce26689441005e47e5fa968fa936257d2c6cb6c7
pupixipup <rkbitinas@gmail.com>
2022-04-13 17:56:47 +0200

    feat: extended repeater solve

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,10 +15,35 @@ const { NotImplementedError } = require('../extensions/index.js');
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater() {
-  throw new NotImplementedError('Not implemented');
+ function repeater(
+  str, 
+  options = { repeatTimes: 1, separator: '+', addition: '', additionRepeatTimes: 0, additionSeparator: '' }) 
+  {
+  const defaultOptions = Object.assign({repeatTimes: 1, separator: '+', addition: '', additionRepeatTimes: 1, additionSeparator: ''}, options);
   
-}
+  let word = [];
+  
+  for (let i = 0; i < defaultOptions.repeatTimes; i++) {
+  word.push(str.toString())
+    for (let j = 0; j < defaultOptions.additionRepeatTimes; j++) {
+    word.push(defaultOptions.addition.toString())
+    if (j != defaultOptions.additionRepeatTimes - 1) {
+    word.push(defaultOptions.additionSeparator.toString())
+    }
+    }
+    word.push(defaultOptions.separator.toString())
+    }
+    if(word[word.length - 1] == defaultOptions.separator) {
+    word.pop();
+    }
+    if(word[word.length - 1] == defaultOptions.additionSeparator) {
+    word.pop();
+    }
+    return word.join('');
+  }
+  
+
+
 
 module.exports = {
   repeater
