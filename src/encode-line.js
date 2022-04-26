1675efd94cfe2d14adc94f38253a2495ad8d0d83
pupixipup <rkbitinas@gmail.com>
2022-04-26 11:02:22 +0200

    feat: solve encode-line task

--- src/encode-line.js
+++ src/encode-line.js
@@ -10,10 +10,25 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
-}
+ function encodeLine(str) {
+
+  str = str.split('');
+  let string = '';
+  let counter = 1;
+  for (let i = 0; i < str.length; i++) {
+  if (str[i] == str[i+1]) {
+    counter = counter + 1;
+  }
+  else if(str[i] != str[i+1]) {
+  
+  let count = (counter > 1) ? counter: '';
+  string += count + str[i];
+  counter = 1;
+  }
+  }
+  return string;
+  }
+  
 
 module.exports = {
   encodeLine
--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,35 +1,195 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Implement class VigenereCipheringMachine that allows us to create
  * direct and reverse ciphering machines according to task description
- * 
+ *
  * @example
- * 
+ *
  * const directMachine = new VigenereCipheringMachine();
- * 
+ *
  * const reverseMachine = new VigenereCipheringMachine(false);
- * 
+ *
  * directMachine.encrypt('attack at dawn!', 'alphonse') => 'AEIHQX SX DLLU!'
- * 
+ *
  * directMachine.decrypt('AEIHQX SX DLLU!', 'alphonse') => 'ATTACK AT DAWN!'
- * 
+ *
  * reverseMachine.encrypt('attack at dawn!', 'alphonse') => '!ULLD XS XQHIEA'
- * 
+ *
  * reverseMachine.decrypt('AEIHQX SX DLLU!', 'alphonse') => '!NWAD TA KCATTA'
- * 
+ *
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor(type = true) {
+    this.type = type;
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  encrypt(message, key) {
+    if (!message || !key) {
+      throw new Error("Incorrect arguments!");
+    }
+
+    let spaces = [];
+    message.split("").forEach(function (item, index) {
+      if (item == " ") {
+        spaces.push(index);
+      }
+    });
+    // Save Spaces
+
+    message = message.toUpperCase();
+
+    key = key.toUpperCase();
+    let alphabet = [
+      "a",
+      "b",
+      "c",
+      "d",
+      "e",
+      "f",
+      "g",
+      "h",
+      "i",
+      "j",
+      "k",
+      "l",
+      "m",
+      "n",
+      "o",
+      "p",
+      "q",
+      "r",
+      "s",
+      "t",
+      "u",
+      "v",
+      "w",
+      "x",
+      "y",
+      "z",
+    ];
+    // Creating a Key Word
+    let keyWord = "";
+    message = message.replace(/\s/g, "");
+    // remove all spaces
+    while (keyWord.length < message.length) {
+      keyWord += key;
+    }
+    keyWord = keyWord.split("");
+    while (keyWord.length > message.length) {
+      keyWord.pop();
+    }
+    keyWord = keyWord.join("");
+    // Creating a Key Word
+
+    let arr = [];
+    let encryptedChar = "";
+    for (let i = 0; i < message.length; i++) {
+      arr.push((message[i].charCodeAt(0) + keyWord[i].charCodeAt(0) + 26) % 26);
+      if (!alphabet.includes(message[i].toLowerCase())) {
+        encryptedChar += message[i];
+      } else {
+        encryptedChar +=
+          alphabet[(message[i].charCodeAt(0) + keyWord[i].charCodeAt(0)) % 26];
+        encryptedChar = encryptedChar.toUpperCase();
+      }
+    }
+    encryptedChar = encryptedChar.split("");
+    for (let space of spaces) {
+      encryptedChar.splice(space, 0, " ");
+    }
+
+    if (this.type) {
+      return encryptedChar.join("");
+    }
+    return encryptedChar.reverse().join("");
+
+    // Add all removed spaces
+  }
+
+  decrypt(message, key) {
+    if (!message || !key) {
+      throw new Error("Incorrect arguments!");
+    }
+
+    let spaces = [];
+    message.split("").forEach(function (item, index) {
+      if (item == " ") {
+        spaces.push(index);
+      }
+    });
+    
+    // Save Spaces
+
+    message = message.replace(/\s/g, "");
+    // remove all spaces
+
+    message = message.toUpperCase();
+    key = key.toUpperCase();
+    let alphabet = [
+      "a",
+      "b",
+      "c",
+      "d",
+      "e",
+      "f",
+      "g",
+      "h",
+      "i",
+      "j",
+      "k",
+      "l",
+      "m",
+      "n",
+      "o",
+      "p",
+      "q",
+      "r",
+      "s",
+      "t",
+      "u",
+      "v",
+      "w",
+      "x",
+      "y",
+      "z",
+    ];
+    // Creating a Key Word
+    let keyWord = "";
+    while (keyWord.length < message.length) {
+      keyWord += key;
+    }
+    keyWord = keyWord.split("");
+    while (keyWord.length > message.length) {
+      keyWord.pop();
+    }
+    keyWord = keyWord.join("");
+    /* return keyWord; */
+    // Creating a Key Word
+    let encryptedChar = "";
+    for (let i = 0; i < message.length; i++) {
+      if (!alphabet.includes(message[i].toLowerCase())) {
+        encryptedChar += message[i];
+      } else {
+        encryptedChar +=
+          alphabet[
+            (message[i].charCodeAt(0) - keyWord[i].charCodeAt(0) + 26) % 26
+          ];
+        encryptedChar = encryptedChar.toUpperCase();
+      }
+    }
+    encryptedChar = encryptedChar.split("");
+    for (let space of spaces) {
+      encryptedChar.splice(space, 0, " ");
+    }
+
+    if (this.type) {
+      return encryptedChar.join("");
+    }
+    return encryptedChar.reverse().join("");
   }
 }
 
 module.exports = {
-  VigenereCipheringMachine
+  VigenereCipheringMachine,
 };
